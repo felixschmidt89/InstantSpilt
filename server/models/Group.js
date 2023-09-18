e9d5cd7a94d942eb8718df70141fc4e349d0dcde
felixschmidt89 <felix.schmidt@dci.education>
2023-09-18 10:35:32 +0200

    added Expenses.js

--- server/models/Group.js
+++ server/models/Group.js
@@ -6,7 +6,7 @@ const groupSchema = new Schema(
       type: String,
       required: true, // ensure that no group can be created wo/ groupID as it is the key value in the application
     },
-    groupname: {
+    groupName: {
       type: String,
       required: true,
     },
