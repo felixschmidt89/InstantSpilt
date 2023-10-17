8c1a5be22d2f9cd6743f520baab110375a857127
felixschmidt89 <felix.schmidt@dci.education>
2023-10-17 20:28:35 +0200

    improved terms and conditions copies

--- client/src/contents/termsAndConditionsContent.js
+++ client/src/contents/termsAndConditionsContent.js
@@ -13,9 +13,9 @@ const termsAndConditionsContent = [
   },
   {
     key: "section3",
-    title: "Legal Compliance",
+    title: "No Cookies, No Tracking",
     content:
-      "When using InstantSplit, you agree to comply with all applicable laws and regulations, including copyright and intellectual property laws.",
+      "We are committed to being as user-friendly and data sensitive as possible. We do not use cookies or tracking mechanisms on our website or application to ensure your privacy all times.",
   },
   {
     key: "section4",
@@ -27,52 +27,52 @@ const termsAndConditionsContent = [
     key: "section5",
     title: "Local Storage",
     content:
-      "InstantSplit utilizes local storage within your browser to provide a tailored and convenient user experience. This local storage primarily houses your associated groupCode(s), ensuring seamless functionality without the need for personal identification or registration. In addition, user settings may be stored in local storage to enhance your interaction with the application. When you depart from a group within the application, any related data in local storage will be removed. You also have the option to manually delete this data, although this action will result in losing access to the group.",
+      "InstantSplit utilizes local storage within your browser to provide a convenient user experience. This local storage primarily houses your associated groupCode(s), ensuring seamless functionality without the need for personal identification or registration. In addition, user settings may be stored in local storage to enhance your interaction with the application. When you depart from a group within the application, any related data in local storage will be removed. You also have the option to manually delete this data in your browser's settings, although this action will result in losing access to the group.",
   },
   {
     key: "section6",
     title: "GroupCode Accessibility",
     content:
-      "Please note that there is no way to restore a groupCode. If you and all group members lose it, the group will not be accessible.",
+      "Please note that there is no way to restore a groupCode. If you and all other group members lose it, the group will no longer be accessible. We can not provide support in such situations as we - by design - can not identify our users. This means that your data is ultimately in your hands, and if you happen to lose access to it, it's irrecoverable and will be purged (see Data Purge).",
   },
   {
     key: "section7",
     title: "Sharing GroupCodes",
     content:
-      "Be advised to share the groupCode cautiously because anyone with the groupCode can gain full access to the group.",
+      "Be advised to share the groupCode cautiously because anyone with the groupCode can gain full access to your group.",
   },
   {
-    key: "section8",
+    key: "section9",
     title: "Indemnification",
     content:
       "You agree to indemnify and hold harmless the developers and contributors of InstantSplit from any claims, losses, or liabilities arising from your use of the application.",
   },
   {
-    key: "section9",
+    key: "section10",
     title: "Termination",
     content:
-      "We reserve the right to terminate your access to the InstantSplit application at our discretion and without notice.",
+      "We reserve the right to terminate access to the InstantSplit application at our discretion and without notice.",
   },
   {
-    key: "section10",
+    key: "section11",
     title: "Data Purge",
     content:
       "As a data-sensitive application, we have implemented a data purge policy. If a group remains inactive for a period of 6 months, all its usage data will be permanently purged. This policy is designed to respect your privacy and ensure that only relevant and active data is retained within the application.",
   },
   {
-    key: "section11",
+    key: "section12",
     title: "No Warranty",
     content:
       "We make no warranties or representations about the suitability, reliability, availability, timeliness, and accuracy of the information, software, and services provided by InstantSplit.",
   },
   {
-    key: "section12",
+    key: "section13",
     title: "Limitation of Liability",
     content:
       "The developers and contributors of InstantSplit shall not be held liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including, but not limited to, damages for loss of data, profits, use, goodwill, or other intangible losses.",
   },
   {
-    key: "section13",
+    key: "section14",
     title: "Entire Agreement",
     content:
       "These terms and conditions constitute the entire agreement between you and InstantSplit concerning your use of the application, superseding any prior or contemporaneous agreements or understandings.",
